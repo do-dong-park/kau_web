ed87107470431943d0feab52c6ebdba2b354b056
ssemee <tpals555@daum.net>
2021-12-29 02:30:12 +0900

    ksm

---
+++ gallery.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>토성마을 - Gallery Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/templatemo.css">
+
+
+    <!-- Load fonts style after rendering the layout styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/about.css">
+    <!-- <link rel="stylesheet" href="assets/css/css_gallery/bootstrap.min.css">                                       -->
+    <link rel="stylesheet" href="assets/css/css_gallery/hero-slider-style.css">                              
+    <link rel="stylesheet" href="assets/css/css_gallery/magnific-popup.css">                                 
+    <link rel="stylesheet" href="assets/css/css_gallery/templatemo-style.css">                                   
+
+    <script src="assets/js/jquery-1.11.3.min.js"></script>
+    <!--
+
+
+TemplateMo 559 Zay Shop
+
+https://templatemo.com/tm-559-zay-shop
+
+-->
+</head>
+
+<body>
+
+<?php
+include 'base/navbar.php';
+?>
+
+<div class="cd-hero"> 
+
+<ul class="cd-hero-slider">
+
+    <!-- Page 1 Gallery One -->
+    <li class="selected">                    
+        <div class="cd-full-width">
+            <div class="container-fluid js-tm-page-content" data-page-no="1" data-page-type="gallery">
+                <div class="tm-img-gallery-container">
+                    <div class="tm-img-gallery gallery-one">
+                    <!-- Gallery One pop up connected with JS code below -->
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g1.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">☕
+                                    토성마을 신메뉴 안내
+                                    겨울의 정원 마쉬멜로우 핫초코를
+                                    새롭게 출시합니다. 따듯한 핫초코와
+                                    크리스마스를 보내시는 건 어떠신가요?
+                                    </p>
+                                    <a href="./assets/img/g1.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g2.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">🏕
+                                    예쁜 삼각집에서 사랑하는 사람과
+                                    아늑한 크리스마스를 보내시는건 어떤가요?
+                                    </p>
+                                    <a href="./assets/img/g2.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>     
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g3.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">다양한 디저트들과 따듯한 음료가 가득한 토성마을로 놀러오세요❤</p>
+                                    <a href="./assets/img/g3.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>                              
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g4.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">비가와도 눈이와도
+                                    아늑하게 지낼수 있는 삼각집에서
+                                    여유로운 휴식의 시간 보내시는건
+                                    어떤가요?
+                                    </p>
+                                    <a href="./assets/img/g4.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>                     
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g5.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">가을하늘의 노을이
+                                    아름다운 카페 #토성마을
+                                    여유롭게 커피한잔과 함께
+                                    행복한 시간 보내시는건 어떤가요?🧡
+                                    </p>
+                                    <a href="./assets/img/g5.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g6.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">🏕
+                                    소중한 이야기를 나누고 싶을땐
+                                    아늑한 삼각집이 있는
+                                    #토성마을 로 놀러오세요🤗
+                                    </p>
+                                    <a href="./assets/img/g6.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g7.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">🛤
+                                    가을하늘이 아름다운 날
+                                    따스한 햇살 아래에서
+                                    따듯한 커피와 달콤한 디저트와 함께
+                                    여유로운 주말을 보내시는건 어떤가요?
+                                    </p>
+                                    <a href="./assets/img/g7.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g8.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">🏕
+                                    어느새 멋진
+                                    삼각형이 된 NEW 삼각집⛰
+                                    </p>
+                                    <a href="./assets/img/g8.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g9.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">🛤
+                                    어느새 찾아온 가을
+                                    여유롭게 주말을 보내고 싶을땐
+                                    한적한 #토성마을 에서
+                                    따듯한 커피한잔 어떠신가요?😊
+                                    </p>
+                                    <a href="./assets/img/g9.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g10.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">빨간 국화와 팜파스 그라스가
+                                    가득한 #토성마을 에
+                                    가을이 찾아왔어요😊.
+                                    </p>
+                                    <a href="./assets/img/g10.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g11.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">🛤
+                                    가을하늘이 아름다운
+                                    토성마을에서 따듯한 커피한잔
+                                    어떠신가요?
+                                    </p>
+                                    <a href="./assets/img/g11.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g12.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">홍차의 은은한 향과
+                                    달콤한 꿀이 만나 만들어진
+                                    #토성마을 수제 밀크티
+                                    </p>
+                                    <a href="./assets/img/g12.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g13.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">가을하늘이 아름다운 이곳
+                                    #토성마을 은 특히나 노을지는 시간에
+                                    오시면 더 멋진 사진을 찍으실 수 있답니다.❤
+                                    </p>
+                                    <a href="./assets/img/g13.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g14.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">가을하늘이 너무 예쁜 요즘
+                                    어딘가로 떠나고 싶을땐
+                                    가까운 #토성마을 로 놀러오세요💚
+                                    </p>
+                                    <a href="./assets/img/g14.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g15.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">가을이 온듯 선선한 주말, 토성마을에서 자연과 함께 힐링 어떨까요 ◡̈ ?</p>
+                                    <a href="./assets/img/g15.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                        <div class="grid-item">
+                            <figure class="effect-sadie">
+                                <img src="./assets/img/g16.jpg" alt="Image" class="img-fluid tm-img">
+                                <figcaption>
+                                    <p class="tm-figure-description">조금씩 가을이 찾아오는게
+                                    느껴지는 요즘 편안한 #토성마을 에서
+                                    따듯한 커피와 함께 가을을 느껴보아요.
+                                    </p>
+                                    <a href="./assets/img/g16.jpg">View more</a>
+                                </figcaption>           
+                            </figure>
+                        </div>
+                    </div>                                 
+                </div>
+            </div>                                                    
+        </div>                    
+    </li>
+
+<!-- Start Script -->
+<script src="assets/js/jquery-1.11.0.min.js"></script>
+<script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/templatemo.js"></script>
+<script src="assets/js/custom.js"></script>
+<script src="assets/js/tether.min.js"></script> 
+<script src="assets/js/bootstrap.min.js"></script>       
+<script src="assets/js/hero-slider-main.js"></script>       
+<script src="assets/js/jquery.magnific-popup.min.js"></script> 
+        
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=3a205c60e9d6eeba49ad7c3159f1d235&libraries=services"></script>
+<script>
+    function adjustHeightOfPage(pageNo) {
+
+        var pageContentHeight = 0;
+
+        var pageType = $('div[data-page-no="' + pageNo + '"]').data("page-type");
+
+        if( pageType != undefined && pageType == "gallery") {
+            pageContentHeight = $(".cd-hero-slider li:nth-of-type(" + pageNo + ") .tm-img-gallery-container").height();
+        }
+        else {
+            pageContentHeight = $(".cd-hero-slider li:nth-of-type(" + pageNo + ") .js-tm-page-content").height() + 20;
+        }
+    
+        // Get the page height
+        var totalPageHeight = $('.cd-slider-nav').height()
+                                + pageContentHeight
+                                + $('#tempaltemo_footer').outerHeight();
+
+        // Adjust layout based on page height and window height
+        if(totalPageHeight > $(window).height()) 
+        {
+            $('.cd-hero-slider').addClass('small-screen');
+            $('.cd-hero-slider li:nth-of-type(' + pageNo + ')').css("min-height", totalPageHeight + "px");
+        }
+        else 
+        {
+            $('.cd-hero-slider').removeClass('small-screen');
+            $('.cd-hero-slider li:nth-of-type(' + pageNo + ')').css("min-height", "100%");
+        }
+    }
+
+    /*
+        Everything is loaded including images.
+    */
+    $(window).load(function(){
+
+        adjustHeightOfPage(1); // Adjust page height
+
+        /* Gallery One pop up
+        -----------------------------------------*/
+        $('.gallery-one').magnificPopup({
+            delegate: 'a', // child items selector, by clicking on it popup will open
+            type: 'image',
+            gallery:{enabled:true}                
+        });
+        
+        /* Gallery Two pop up
+        -----------------------------------------*/
+        $('.gallery-two').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery:{enabled:true}                
+        });
+
+        /* Gallery Three pop up
+        -----------------------------------------*/
+        $('.gallery-three').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery:{enabled:true}                
+        });
+
+        /* Collapse menu after click 
+        -----------------------------------------*/
+        $('#tmNavbar a').click(function(){
+            $('#tmNavbar').collapse('hide');
+
+            adjustHeightOfPage($(this).data("no")); // Adjust page height       
+        });
+
+        /* Browser resized 
+        -----------------------------------------*/
+        $( window ).resize(function() {
+            var currentPageNo = $(".cd-hero-slider li.selected .js-tm-page-content").data("page-no");
+            
+            // wait 3 seconds
+            setTimeout(function() {
+                adjustHeightOfPage( currentPageNo );
+            }, 1000);
+            
+        });
+
+        // Remove preloader (https://ihatetomatoes.net/create-custom-preloading-screen/)
+        $('body').addClass('loaded');
+
+        // Write current year in copyright text.
+        $(".tm-copyright-year").text(new Date().getFullYear());
+                
+    });
+
+</script>
+
+<!-- End Script -->
+</body>
+
+</html>
